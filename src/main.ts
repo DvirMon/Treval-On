ffc6cda0b0885cdfa04f4c76d94be5c6c86c924b
DvirMon <dmenajem@gmail.com>
2023-06-08 18:12:25 +0300

    feat () - add @ngrx/store-devtools

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import { importProvidersFrom } from '@angular/core';
+import { importProvidersFrom, isDevMode } from '@angular/core';
 import { AppComponent } from './app/app.component';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app/app.routes';
 import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 
 bootstrapApplication(AppComponent, {
@@ -12,7 +13,8 @@ bootstrapApplication(AppComponent, {
     importProvidersFrom(BrowserModule),
     provideRouter(appRoutes),
     provideAnimations(),
-    provideStore()
+    provideStore(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })
 ]
 })
   .catch(err => console.error(err));
