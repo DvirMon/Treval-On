d76b4ea381277978da2d4019272dc42ebb945c87
DvirMon <dmenajem@gmail.com>
2023-06-08 17:30:48 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'to-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'travel-on';
+}
