449d91f9d2daa9df0d11d5bb206797ea5194460c
DvirMon <dmenajem@gmail.com>
2023-06-08 17:49:42 +0300

    feat - add app.routes.ts

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,16 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-
 import { importProvidersFrom } from '@angular/core';
 import { AppComponent } from './app/app.component';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { AppRoutingModule } from './app/app-routing.module';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { appRoutes } from './app/app.routes';
 
 
 bootstrapApplication(AppComponent, {
-    providers: [
-        importProvidersFrom(BrowserModule, AppRoutingModule),
-        provideAnimations()
-    ]
+  providers: [
+    importProvidersFrom(BrowserModule),
+    provideRouter(appRoutes),
+    provideAnimations()
+  ]
 })
   .catch(err => console.error(err));
