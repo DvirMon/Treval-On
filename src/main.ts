a497ceca0170e3eac209d407419160f17c3e68be
DvirMon <dmenajem@gmail.com>
2023-06-08 18:10:09 +0300

    feat () - add @ngrx/store

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,15 @@ import { provideAnimations } from '@angular/platform-browser/animations';
 import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app/app.routes';
+import { provideStore } from '@ngrx/store';
 
 
 bootstrapApplication(AppComponent, {
   providers: [
     importProvidersFrom(BrowserModule),
     provideRouter(appRoutes),
-    provideAnimations()
-  ]
+    provideAnimations(),
+    provideStore()
+]
 })
   .catch(err => console.error(err));
