f57d66deb442ea8f31e908db7e3fcaab0db247ca
DvirMon <dmenajem@gmail.com>
2023-06-11 17:10:14 +0300

    feat (environment.ts) - add file

---
+++ src/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  // server : "https://react-vacation-app.herokuapp.com",
+  server: "http://localhost:3000",
+  imageBucket: "https://vacations-image-bucket.s3.eu-west-3.amazonaws.com",
+  CLIENT_ID: '201775417836-mij0ggqv6dh9r9p0r396qvevgnopq8pi.apps.googleusercontent.com'
+}
+
