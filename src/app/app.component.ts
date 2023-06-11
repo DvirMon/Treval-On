fe9a687767fc7ddc06ceb234ea8b437850f12cce
DvirMon <dmenajem@gmail.com>
2023-06-11 12:52:47 +0300

    feat () - add files to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,48 +1,17 @@
-import { ChangeDetectionStrategy, Component, Signal, WritableSignal, computed, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { Vacation } from './vacation.model';
-import { CommonModule } from '@angular/common';
-import { arrayToRecord } from './utilities/helpers';
-import { VacationCardComponent } from './components/vacation-item/vacation-card.component';
-import { SelectChangedEvent } from './components/vacation-item-button/vacation-card-button.component';
+import { Component, ChangeDetectionStrategy } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+
 
 @Component({
   selector: 'to-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [RouterOutlet, VacationCardComponent, CommonModule],
+  imports: [RouterOutlet,],
   changeDetection: ChangeDetectionStrategy.OnPush
 
 })
 export class AppComponent {
   title = 'travel-on';
 
-  readonly vacations: Vacation[] = [{ id: '1' }, { id: '2' }, { id: '3' }, { id: '4' }, { id: '5' }, { id: '6' }]
-
-  readonly selectedVacations: string[] = ['1', '4']
-
-  readonly selectedMap: WritableSignal<Record<string, boolean>> = signal(arrayToRecord(this.selectedVacations))
-
-  onSelectChanged(event: SelectChangedEvent): void {
-    const { source, selected } = event
-    const { vacation } = source
-
-    this.selectedMap.update((state) => {
-
-      if(selected) {
-        state = {
-          ...state,
-          [vacation.id]: selected
-        }
-      }
-
-      if(!selected) {
-
-        delete state[vacation.id]
-      }
-
-      return state
-    })
-  }
 }
---
+++ src/app/pages/user/user.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'to-user',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss']
+})
+export class UserComponent {
+
+}
