6183879c49901e331886c148cfeb67e8f63f6479
DvirMon <dmenajem@gmail.com>
2023-06-12 16:39:05 +0300

    feat (home.component) - remove imports

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,6 @@
 import { Component, Signal, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Observable } from 'rxjs';
 import { Vacation } from 'src/app/store/vacations/vacation.model';
-import { Store } from '@ngrx/store';
-import { VacationSelectors } from 'src/app/store/vacations/vacation.selectors';
 import { StoreService } from 'src/app/store/store.service';
 
 
