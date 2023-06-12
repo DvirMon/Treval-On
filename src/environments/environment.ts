ad16e1a5d097a5b094ddcbd8b3d982e0ba5e0009
DvirMon <dmenajem@gmail.com>
2023-06-12 11:48:26 +0300

    feat () - ngg add @angular/fire

---
+++ src/environments/environment.ts
@@ -0,0 +1,28 @@
+export const environment = {
+  production: true,
+  // server : "https://react-vacation-app.herokuapp.com",
+  server: "http://localhost:3000",
+  imageBucket: "https://vacations-image-bucket.s3.eu-west-3.amazonaws.com",
+  CLIENT_ID: '201775417836-mij0ggqv6dh9r9p0r396qvevgnopq8pi.apps.googleusercontent.com',
+  firebaseConfig: {
+    apiKey: "AIzaSyAWE61Vm0CpfUtHq4G48aJVMbdY6REEtrA",
+    authDomain: "travel-on-811d5.firebaseapp.com",
+    databaseURL: "https://travel-on-811d5-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "travel-on-811d5",
+    storageBucket: "travel-on-811d5.appspot.com",
+    messagingSenderId: "443306265288",
+    appId: "1:443306265288:web:0362cd9bd5a605e5d533fd",
+    measurementId: "G-BSDTG44DV2"
+  },
+  firebase: {
+    projectId: 'travel-on-811d5',
+    appId: '1:443306265288:web:0362cd9bd5a605e5d533fd',
+    databaseURL: 'https://travel-on-811d5-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'travel-on-811d5.appspot.com',
+    apiKey: 'AIzaSyAWE61Vm0CpfUtHq4G48aJVMbdY6REEtrA',
+    authDomain: 'travel-on-811d5.firebaseapp.com',
+    messagingSenderId: '443306265288',
+    measurementId: 'G-BSDTG44DV2'
+  }
+}
+
--- src/main.ts
+++ src/main.ts
@@ -7,16 +7,36 @@ import { appRoutes } from './app/app.routes';
 import { provideStore } from '@ngrx/store';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
 import { provideEffects } from '@ngrx/effects';
+import { provideAnalytics, getAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideMessaging, getMessaging } from '@angular/fire/messaging';
+import { environment } from './environments/environment';
 
 
 bootstrapApplication(AppComponent, {
   providers: [
-    importProvidersFrom(BrowserModule),
+    importProvidersFrom
+      (
+        BrowserModule,
+        provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+        provideAnalytics(() => getAnalytics()),
+        provideAuth(() => getAuth()),
+        provideFirestore(() => getFirestore()),
+        provideMessaging(() => getMessaging())
+      ),
     provideRouter(appRoutes),
     provideAnimations(),
     provideStore(),
     provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
-    provideEffects()
-]
+    provideEffects(),
+    {
+      provide: ScreenTrackingService, useClass: ScreenTrackingService
+    },
+    {
+      provide: UserTrackingService, useClass: UserTrackingService
+    }
+  ]
 })
   .catch(err => console.error(err));
