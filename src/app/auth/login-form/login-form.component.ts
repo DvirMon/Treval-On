20448961e97b3b35de167a27ba51d9f2ebc896e6
DvirMon <dmenajem@gmail.com>
2023-06-23 15:10:16 +0300

    feat (login-form) - register navigation works

--- src/app/auth/login-form/login-form.component.ts
+++ src/app/auth/login-form/login-form.component.ts
@@ -6,17 +6,18 @@ import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import { AuthService } from '../auth.service';
-import { EMPTY, Observable, Subject, catchError, exhaustMap } from 'rxjs';
-import { User } from 'src/app/store/user/user.model';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { RouterModule } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { EMPTY, Observable, Subject, catchError, exhaustMap } from 'rxjs';
+import { User } from 'src/app/store/user/user.model';
 
 @Component({
   selector: 'to-login-form',
   standalone: true,
-  imports: [CommonModule, FormsModule, ReactiveFormsModule, NgOptimizedImage, MatFormFieldModule, MatInputModule, MatCardModule, MatButtonModule, MatIconModule, MatDividerModule],
+  imports: [CommonModule, RouterModule, FormsModule, ReactiveFormsModule, NgOptimizedImage, MatFormFieldModule, MatInputModule, MatCardModule, MatButtonModule, MatIconModule, MatDividerModule],
   templateUrl: './login-form.component.html',
   styleUrls: ['./login-form.component.scss']
 })
@@ -40,6 +41,7 @@ export class LoginFormComponent {
   protected onButtonClick(): void {
     this.loginSource.next();
   }
+
 
   private _signInWithGoogle$(): Observable<User> {
     return this.loginSource.asObservable().pipe(
