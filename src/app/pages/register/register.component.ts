bb86ef16b3185c9652f3e719aa1dcfc3a53c5ca1
DvirMon <dmenajem@gmail.com>
2023-06-25 11:32:15 +0300

    feat () - remove otp component

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -2,12 +2,11 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RegisterFormComponent } from 'src/app/auth/register-form/register-form.component';
 import { FloatingButtonComponent } from 'src/app/components/floating-button/floating-button.component';
-import { OtpComponent } from 'src/app/components/otp/otp.component';
 
 @Component({
   selector: 'to-register',
   standalone: true,
-  imports: [CommonModule, RegisterFormComponent, FloatingButtonComponent, OtpComponent],
+  imports: [CommonModule, RegisterFormComponent, FloatingButtonComponent],
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss'],
   changeDetection : ChangeDetectionStrategy.OnPush
