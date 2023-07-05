ea61068bc89146b676b937d193d787abe52d7066
DvirMon <dmenajem@gmail.com>
2023-07-05 15:54:58 +0300

    feat (otp-form) - add component

---
+++ src/app/auth/otp-form/otp-form.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'to-otp-form',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './otp-form.component.html',
+  styleUrls: ['./otp-form.component.scss']
+})
+export class OtpFormComponent {
+
+}
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,13 +1,14 @@
-import { ChangeDetectionStrategy, Component, WritableSignal, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginFormComponent } from 'src/app/auth/login-form/login-form.component';
 import { FloatingButtonComponent } from 'src/app/components/floating-button/floating-button.component';
 import { FlipCardComponent } from 'src/app/components/flip-card/flip-card.component';
+import { OtpFormComponent } from 'src/app/auth/otp-form/otp-form.component';
 
 @Component({
   selector: 'to-login-page',
   standalone: true,
-  imports: [CommonModule, FlipCardComponent, LoginFormComponent, FloatingButtonComponent],
+  imports: [CommonModule, FloatingButtonComponent, FlipCardComponent, LoginFormComponent, OtpFormComponent],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
