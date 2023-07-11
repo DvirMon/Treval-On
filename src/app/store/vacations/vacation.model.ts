e381e660dcb5924102b02f4a4dcf3202add23b66
DvirMon <dmenajem@gmail.com>
2023-07-11 11:05:03 +0300

    feat (vacation.model) - update Vacation Model with Timestamp

--- src/app/store/vacations/vacation.model.ts
+++ src/app/store/vacations/vacation.model.ts
@@ -1,14 +1,17 @@
+import { Timestamp } from '@angular/fire/firestore';
 
 export interface Destination {
   city : string,
   country : string,
 }
 
+// export interface Timestamp
+
 export interface Vacation {
   id: string,
   destination: Destination,
   price: number,
-  takeoff : Date,
-  landing: Date,
+  takeoff :  Timestamp,
+  landing: Timestamp,
   imageUrl: string
 }
