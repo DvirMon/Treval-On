d32cb3109d546e3916613dea44a1e7348538ab92
DvirMon <dmenajem@gmail.com>
2023-07-18 09:35:26 +0300

    feat (email-link.ts) - copy logic from form-contact

--- src/app/auth/email-link-form/email-link-form.component.ts
+++ src/app/auth/email-link-form/email-link-form.component.ts
@@ -1,16 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ContactFormComponent, MessageType } from '../contact-form/contact-form.component';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTabsModule } from '@angular/material/tabs';
+import { SignInEvent, SignInMethod } from '../store/auth.model';
+
+
 
 @Component({
   selector: 'to-email-link-form',
   standalone: true,
-  imports: [CommonModule, ContactFormComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatTabsModule,
+    MatButtonModule
+  ],
   templateUrl: './email-link-form.component.html',
   styleUrls: ['./email-link-form.component.scss']
 })
 export class EmailLinkFormComponent {
 
-  type: MessageType = MessageType.EMAIL
+  @Output() contactSubmit: EventEmitter<SignInEvent> = new EventEmitter();
+
+  protected formControl!: FormControl<string>
+
+
+
+  protected onSubmit(value: string): void {
+    const event: SignInEvent = { method: SignInMethod.EMAIL_LINK, data: value }
+    this.contactSubmit.emit(event)
+  }
 
 }
