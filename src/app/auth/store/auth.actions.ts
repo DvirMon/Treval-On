c87fa8fbb605ffd336c5a2cdb91d5d109a9b9ecb
DvirMon <dmenajem@gmail.com>
2023-07-18 10:15:14 +0300

    feat (auth.store) - add send link action

--- src/app/auth/store/auth.actions.ts
+++ src/app/auth/store/auth.actions.ts
@@ -6,8 +6,7 @@ export const AuthActions = createActionGroup({
   source: 'Auth/API',
   events: {
     'Sign In': props<{ signInEvent: SignInEvent }>(),
-    'Sign In With Gmail': emptyProps(),
-    'Sign In With Email And Password': props<{ login: EmailAndPasswordSignIn }>(),
+    'Send Email Link': props<{ email : string }>(),
     'Load User Success': props<{ user: User }>(),
     'Add User': props<{ register: Register }>(),
     'Update User': props<{ register: Register }>(),
