f48f0d7d7ff14ea7a7b0cd4fe30c00257b6418e3
DvirMon <dmenajem@gmail.com>
2023-08-03 11:39:37 +0300

    feat () - update firebase yml

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,30 @@
-const path = require('path');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
-  mode: 'development', // or 'production' as per your requirement
-  entry: './src/main.ts', // The path to your main TypeScript file
+  entry: './src/main.ts', // Entry point of your application
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js', // The name of the output bundle
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
+    path: __dirname + '/dist', // Output directory for the build artifacts
+    filename: 'bundle.js', // Output bundle file name
   },
   module: {
     rules: [
+      // Define your Webpack loaders for different file types (e.g., TypeScript, CSS, etc.)
       {
         test: /\.ts$/,
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      // Add more loaders for CSS, images, etc. as needed
     ],
   },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  plugins: [
+    // Add the BundleAnalyzerPlugin to your plugins array
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static', // Generate a static HTML report file
+      reportFilename: 'bundle-report.html', // Output report file name
+    }),
+  ],
 };
