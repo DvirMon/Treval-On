7410ca2de5e939a5e1790d332216da3cd529d5c7
DvirMon <dmenajem@gmail.com>
2023-08-17 13:38:47 +0300

    feat (register) - working on register form

--- src/app/auth/register-form/register-form.component.ts
+++ src/app/auth/register-form/register-form.component.ts
@@ -1,15 +1,53 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { FormGroup, NonNullableFormBuilder, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'to-register-form',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatCardModule],
   templateUrl: './register-form.component.html',
   styleUrls: ['./register-form.component.scss'],
-  changeDetection : ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush
 
 })
 export class RegisterFormComponent {
+
+
+
+
+
+
+  private _buildRegisterForm() {
+    return inject(NonNullableFormBuilder).group({
+      firstName: ['', [
+        Validators.required,
+        // Validators.pattern(this.validationService.regex.name),
+        Validators.minLength(3),
+        Validators.maxLength(30)]],
+      lastName: ['', [
+        Validators.required,
+        // Validators.pattern(this.validationService.regex.name),
+        Validators.minLength(3),
+        Validators.maxLength(30)]],
+      email: ['',
+        // [Validators.required, Validators.pattern(this.validationService.regex.email)],
+        // [this.validationService.emailUniqueAsyncValidation.bind(this)]
+      ],
+      password: ['', [
+        Validators.required,
+        // Validators.pattern(this.validationService.regex.password),
+        Validators.minLength(8),
+        Validators.maxLength(24)]],
+      // confirmPassword: ['',
+      //   [Validators.required, Validators.pattern(this.validationService.regex.password)]],
+    },
+      {
+        // validator: [this.validationService.mustMatch('password', 'confirmPassword')],
+      })
+  }
+
 
 }
