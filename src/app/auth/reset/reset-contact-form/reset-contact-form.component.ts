658b425e8457b594ccc50f55b3bcb1e809789f3d
dvir.monajem <dmenajem@gmail.com>
2024-03-08 16:01:08 +0200

    reset - add reset password form

--- src/app/auth/reset/reset-contact-form/reset-contact-form.component.ts
+++ src/app/auth/reset/reset-contact-form/reset-contact-form.component.ts
@@ -1,3 +1,4 @@
+import { JsonPipe, TitleCasePipe } from "@angular/common";
 import {
   Component,
   EventEmitter,
@@ -5,7 +6,6 @@ import {
   WritableSignal,
   inject,
 } from "@angular/core";
-import { CommonModule, JsonPipe, TitleCasePipe } from "@angular/common";
 import {
   FormControl,
   FormGroup,
@@ -13,13 +13,13 @@ import {
   ReactiveFormsModule,
   Validators,
 } from "@angular/forms";
-import { EmailAndPasswordSignIn } from "../../store/auth.model";
-import { getFormKeys } from "src/app/components/form-input/form.helper";
 import { MatButton } from "@angular/material/button";
 import { MatCard, MatCardContent } from "@angular/material/card";
 import { MatIcon } from "@angular/material/icon";
 import { DividerHeaderComponent } from "src/app/components/divider-header/divider-header.component";
 import { FormInputComponent } from "src/app/components/form-input/form-input.component";
+import { getFormKeys } from "src/app/components/form-input/form.helper";
+import { EmailAndPasswordSignIn } from "../../store/auth.model";
 
 interface ResetContactForm {
   email: FormControl<string>;
@@ -46,7 +46,7 @@ export class ResetContactFormComponent {
   public readonly resetFormGroup: FormGroup<ResetContactForm>;
   public readonly formKeys: WritableSignal<string[]>;
 
-  @Output() resetPassword: EventEmitter<string> = new EventEmitter();
+  @Output() sendContact: EventEmitter<string> = new EventEmitter();
 
   constructor() {
     this.resetFormGroup = this._buildResetForm();
@@ -60,6 +60,6 @@ export class ResetContactFormComponent {
   }
 
   public onSubmit(value: Partial<EmailAndPasswordSignIn>): void {
-    this.resetPassword.emit(value as string);
+    this.sendContact.emit(value as string);
   }
 }
--- src/app/auth/reset/reset-password-form/reset-password-form.component.ts
+++ src/app/auth/reset/reset-password-form/reset-password-form.component.ts
@@ -1,11 +1,28 @@
 import { JsonPipe, TitleCasePipe } from "@angular/common";
-import { Component } from "@angular/core";
-import { ReactiveFormsModule } from "@angular/forms";
+import {
+  Component,
+  EventEmitter,
+  Output,
+  WritableSignal,
+  inject,
+} from "@angular/core";
+import {
+  FormControl,
+  FormGroup,
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+  Validators,
+} from "@angular/forms";
 import { MatButton } from "@angular/material/button";
 import { MatCard, MatCardContent } from "@angular/material/card";
 import { MatIcon } from "@angular/material/icon";
 import { DividerHeaderComponent } from "src/app/components/divider-header/divider-header.component";
 import { FormInputComponent } from "src/app/components/form-input/form-input.component";
+import { getFormKeys } from "src/app/components/form-input/form.helper";
+
+interface ResetPasswordForm {
+  newPassword: FormControl<string>;
+}
 
 @Component({
   selector: "to-reset-password-form",
@@ -24,4 +41,31 @@ import { FormInputComponent } from "src/app/components/form-input/form-input.com
   templateUrl: "./reset-password-form.component.html",
   styleUrl: "./reset-password-form.component.scss",
 })
-export class ResetPasswordFormComponent {}
+export class ResetPasswordFormComponent {
+  public readonly resetPasswordFormGroup: FormGroup<ResetPasswordForm>;
+  public readonly formKeys: WritableSignal<string[]>;
+
+  @Output() resetPassword: EventEmitter<string> = new EventEmitter();
+
+  constructor() {
+    this.resetPasswordFormGroup = this._buildResetForm();
+    this.formKeys = getFormKeys(this.resetPasswordFormGroup);
+  }
+
+  private _buildResetForm(): FormGroup<ResetPasswordForm> {
+    return inject(NonNullableFormBuilder).group({
+      newPassword: [
+        "",
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(16),
+        ],
+      ],
+    });
+  }
+
+  public onSubmit(value: Partial<{ newPassword: string }>): void {
+    this.resetPassword.emit(value as string);
+  }
+}
